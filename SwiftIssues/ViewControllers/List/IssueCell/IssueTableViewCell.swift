34c97513fec7493150ff8fd68465ddd81a2bf17b
user917337 <user917337@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-10-05 13:33:53 -0400

    ~firebase~

---
+++ SwiftIssues/ViewControllers/List/IssueCell/IssueTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  IssueTableViewCell.swift
+//  SwiftIssues
+//
+//  Created by marcos.guedes on 10/5/19.
+//  Copyright © 2019 jaya. All rights reserved.
+//
+
+import UIKit
+
+class IssueTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var issueTitleLabel: UILabel!
+    @IBOutlet weak var issueStatusLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
+
+extension IssueTableViewCell {
+    func configure(with title: String, and state: String) {
+        issuesTitleLabel.text? = title
+        issuesStatusLabel.text? = state
+        
+    }
+}
---
+++ SwiftIssues/ViewControllers/List/IssuesViewController.swift
@@ -0,0 +1,36 @@
+//
+//  IssuesViewController.swift
+//  SwiftIssues
+//
+//  Created by marcos.guedes on 10/5/19.
+//  Copyright © 2019 jaya. All rights reserved.
+//
+
+import UIKit
+
+class IssuesViewController: UIViewController {
+    
+    var presenter: IssuesViewPresenter!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension IssuesViewController'lffe`1~1357.,./
+
+
